306c546033b979655a1ee835f1bc16a3b9ec3277
Beatriz Fernandes <lfs.beatriz.leonardo@gmail.com>
2026-07-26 18:42:28 -0300

    organization

--- godot/scenes/ui/main_menu.gd
+++ godot/scenes/ui/main_menu.gd
@@ -2,6 +2,7 @@ extends Control
 
 func _ready() -> void:
 	$Menu/VBoxContainer/jogar.pressed.connect(_on_btjogar_pressed)
+	$Menu/VBoxContainer/continuar.pressed.connect(_on_btjogar_pressed)
 
 func _process(delta: float) -> void:
 	pass
