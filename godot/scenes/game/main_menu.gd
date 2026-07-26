0d89ec697994a8d87af5d6b276793d72a5f1999d
Beatriz Fernandes <lfs.beatriz.leonardo@gmail.com>
2026-07-26 18:40:12 -0300

    play_transition

---
+++ godot/scenes/game/main_menu.gd
@@ -0,0 +1,10 @@
+extends Control
+
+func _ready() -> void:
+	$Menu/VBoxContainer/jogar.pressed.connect(_on_btjogar_pressed)
+
+func _process(delta: float) -> void:
+	pass
+
+func _on_btjogar_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/game/Game.tscn")
